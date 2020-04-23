0044cca52d27ecb2cf2d066d87610685e3331f14
ptol86 <ah021714@gmail.com>
2020-04-23 20:07:45 +0300

    task1

---
+++ gromcodejs/JS lessons/lesson2/task1/index.js
@@ -0,0 +1,12 @@
+/*
+
+const User Name = 'John';
+const 1greeting = "I'm John";
+const _profession = 'Developer';
+const $ho%bby = 'Walking';
+const FavouriteDESSERT = 'cake';
+let pet_ = 'dog';
+let super&Number;
+super&Number = 17;
+
+*/
