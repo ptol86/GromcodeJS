85f3bcf433ad0f72d5daf864bc89c62ba4e475aa
ptol86 <ah021714@gmail.com>
2020-04-23 21:09:44 +0300

    task3

---
+++ gromcodejs/lesson2/task3/index.js
@@ -0,0 +1,13 @@
+goodNumber = 17;
+anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+customer = {
+    name: "John",
+    age: 17
+};
+anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
