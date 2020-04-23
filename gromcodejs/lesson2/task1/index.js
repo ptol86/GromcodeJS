0586ecbbc222f4df21286480432dc76b3f8c76d1
ptol86 <ah021714@gmail.com>
2020-04-23 20:50:22 +0300

    task1

--- gromcodejs/lesson2/task1/index.js
+++ gromcodejs/lesson2/task1/index.js
@@ -1,6 +1,6 @@
 const userName = 'John';
-const greeting1 = "I'm John";
-const profession_ = 'Developer';
+const greeting = "I'm John";
+const _profession = 'Developer';
 const $hobby = 'Walking';
 const favouriteDessert = 'cake';
 let pet_ = 'dog';
