2da2f3760ce5ca000c0e25d8494cd3663f44850f
ptol86 <ah021714@gmail.com>
2020-04-23 21:12:36 +0300

    task3

--- gromcodejs/lesson2/task3/index.js
+++ gromcodejs/lesson2/task3/index.js
@@ -1,13 +1,13 @@
-goodNumber = 17;
-anotherNumber = goodNumber;
+let goodNumber = 17;
+let anotherNumber = goodNumber;
 anotherNumber = 77;
 console.log(goodNumber);
 console.log(anotherNumber);
-customer = {
+let customer = {
     name: "John",
     age: 17
 };
-anotherCustomer = customer;
+let anotherCustomer = customer;
 anotherCustomer.age = 20;
 console.log(customer.age);
 console.log(anotherCustomer.age);
