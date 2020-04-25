6b1226b96500f6bc4ffb4217b5677da5516c1774
ptol86 <ah021714@gmail.com>
2020-04-25 13:46:39 +0300

    task5

--- gromcodejs/lesson4/task5/index.js
+++ gromcodejs/lesson4/task5/index.js
@@ -4,7 +4,7 @@ let result = 0;
 for (let i = m; i <= n; i++)
     if (i % 5 === 0) {
         console.log(i);
-        break;
+
     } else {
         if (i % 2 === 0 && i % 4 !== 0) {
             result += i;
