31f20880ba6c1fe429289865025dbad2ed63d93a
ptol86 <ah021714@gmail.com>
2020-04-25 13:45:58 +0300

    task5

--- gromcodejs/lesson4/task5/index.js
+++ gromcodejs/lesson4/task5/index.js
@@ -1,22 +1,18 @@
 const m = 2;
 const n = 45;
 let result = 0;
-for (let i = m; i <= n; i++) {
+for (let i = m; i <= n; i++)
     if (i % 5 === 0) {
         console.log(i);
-        continue;
+        break;
+    } else {
+        if (i % 2 === 0 && i % 4 !== 0) {
+            result += i;
+        }
+        if (i % 3 === 0) {
+            result -= i;
+        }
+        if (i % 4 === 0) {
+            result *= i;
+        }
     }
-    if (i % 2 === 0 && i % 4 !== 0) {
-        result += i;
-    }
-    if (i % 3 === 0) {
-        result -= i;
-    }
-    if (i % 4 === 0) {
-        result *= i;
-    }
-
-
-
-
-}
