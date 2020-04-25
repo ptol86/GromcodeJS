5ba95c8ec481b9a743b0e8421b81d5be5392de3e
ptol86 <ah021714@gmail.com>
2020-04-25 16:59:02 +0300

    task6

--- gromcodejs/lesson5/task6/index.js
+++ gromcodejs/lesson5/task6/index.js
@@ -1,6 +1,6 @@
 function getPrimes(number) {
-    for (let minNumber = 1; minNumber <= number; minNumber++) {
-        if (minNumber % minNumber === 0 && minNumber % 1 === 0 && minNumber !== 1) {
+    for (let minNumber = 2; minNumber <= number; minNumber++) {
+        if (minNumber % minNumber === 0 && minNumber % 1 === 0) {
             console.log(minNumber);
         }
     }
