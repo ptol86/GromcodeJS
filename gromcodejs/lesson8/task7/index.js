3aad4c1922c1ff032e7270489d2c31532886cd01
ptol86 <ah021714@gmail.com>
2020-05-11 14:56:09 +0300

    test

---
+++ gromcodejs/lesson8/task7/index.js
@@ -0,0 +1,24 @@
+const addPropertyV1 = (obj, key, value) => {
+  
+  obj[key] = value;
+  return obj;
+}
+
+const addPropertyV2 = (obj, key, value) => {
+  
+  Object.assign(obj,{[key]: value});
+  return obj;
+}
+
+const addPropertyV3 = (obj, key, value) => {
+  let newObj = {};
+  Object.assign(newObj, obj, {[key]: value});
+  return newObj;
+}
+const addPropertyV4 = (obj, key, value) => {
+  
+  let newObj = {...obj};
+  newObj[key] = value;
+  return newObj;
+}
+let zalupa = {};
