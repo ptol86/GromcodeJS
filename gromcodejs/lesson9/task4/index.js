453d4ca37db51b46bb88907b690c99516eaf3d31
ptol86 <ah021714@gmail.com>
2020-05-13 21:50:58 +0300

    task

--- gromcodejs/lesson9/task4/index.js
+++ gromcodejs/lesson9/task4/index.js
@@ -1,38 +1,43 @@
 
-
-    rooms = {
-        room1: [
-            { name: 'room1 name1' },
-            { name: 'room1 name2' },
-            { name: 'room1 name3' },
-            { name: 'room1 name4' },
-            
-        ],
-        room2: [
-            { name: 'room2 name1' },
-            
-        ],
-        room3: [
-            { name: 'room3 name1' },
-            { name: 'room3 name2' },
-            { name: 'room3 name3' },
-            
-        ],
+const rooms = {
+    room1: [
+        [ {name: 'room1 name1'}, {name: 'room111 name000'} ],
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
         
-    };
+    ],
+    room2: [
+        { name: 'room2 name1' },
+        21312313
+        
+    ],
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+        
+    ],
+    
+};
 
+
+results = [];
 
 const getPeople = (obj) => {
-    let result = [];
-    result = Object.values(obj);
-    let arr = result.flat();
-    // console.log(arr);
-    let aaa = [];
-    for (let value of arr) {
-        aaa.push(Object.values(value));
+    if ((obj instanceof Array) || (obj instanceof Object)) {
+        for (let key in obj) {
+            getPeople(obj[key])
+        }
     }
-    return aaa.flat();
-
+    else {    
+        results.push(obj);
+}
+    return results;
 };
+
+
+
+
 
 console.log(getPeople(rooms));
