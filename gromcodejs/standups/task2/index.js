4525a4bd5dc18773a631d1af284dc581bf9820b0
ptol86 <ah021714@gmail.com>
2020-05-13 11:02:03 +0300

    task

--- gromcodejs/standups/task2/index.js
+++ gromcodejs/standups/task2/index.js
@@ -7,26 +7,26 @@ for (let i = m; 1 <= n; i++) {
     
     if (i % 5 === 0) {
         result +=1;
-        console.log("4 if is called for number " + i);
+        // console.log("4 if is called for number " + i);
         console.log(i);
         continue;
     }
     
     if (i % 2 === 0 && i % 4 !==0) {
-        console.log("1 if is called for number " + i);
+        // console.log("1 if is called for number " + i);
         result +=1;
         continue;
     }
     if (i % 3 === 0) {
-        console.log("2 if is called for number " + i);
+        // console.log("2 if is called for number " + i);
         result -=1;
         continue;
     }
     if (i % 4 === 0) {
-        console.log("3 if is called for number " + i);
+        // console.log("3 if is called for number " + i);
         result *=1;
         continue;
     }
 
 }
-console.log(result);
+// console.log(result);
