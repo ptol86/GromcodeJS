4c079415e2de56c43173fd7bc92086d3ba3a3a51
ptol86 <ah021714@gmail.com>
2020-05-13 21:29:55 +0300

    task

---
+++ gromcodejs/lesson9/task4/index.js
@@ -0,0 +1,38 @@
+
+
+    rooms = {
+        room1: [
+            { name: 'room1 name1' },
+            { name: 'room1 name2' },
+            { name: 'room1 name3' },
+            { name: 'room1 name4' },
+            
+        ],
+        room2: [
+            { name: 'room2 name1' },
+            
+        ],
+        room3: [
+            { name: 'room3 name1' },
+            { name: 'room3 name2' },
+            { name: 'room3 name3' },
+            
+        ],
+        
+    };
+
+
+const getPeople = (obj) => {
+    let result = [];
+    result = Object.values(obj);
+    let arr = result.flat();
+    console.log(arr);
+    let aaa = [];
+    for (let value of arr) {
+        aaa.push(Object.values(value));
+    }
+    console.log(aaa.flat());
+
+};
+
+console.log(getPeople(rooms));
