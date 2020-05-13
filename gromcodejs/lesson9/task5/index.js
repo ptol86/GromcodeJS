fd53181909a8874e020b34045bc3fdf688b26c10
ptol86 <ah021714@gmail.com>
2020-05-13 22:53:42 +0300

    task

--- gromcodejs/lesson9/task5/index.js
+++ gromcodejs/lesson9/task5/index.js
@@ -1,37 +1,40 @@
 
-const rooms = {
-    room1: [
-        [ {name: 'room1 name1'}, {name: 'room111 name000'} ],
-        { name: 'room1 name2' },
-        { name: 'room1 name3' },
-        { name: 'room1 name4' },
+// const rooms = {
+//     room1: [
+//         { name: 'room1 name1' }, 
+//         { name: 'room1 name2' },
+//         { name: 'room1 name3' },
+//         { name: 'room1 name4' },
         
-    ],
-    room2: [
-        { name: 'room2 name1' },
-        21312313
+//     ],
+//     room2: [
+//         { name: 'room2 name1' },
+       
         
-    ],
-    room3: [
-        { name: 'room3 name1' },
-        { name: 'room3 name2' },
-        { name: 'room3 name3' },
+//     ],
+//     room3: [
+//         { name: 'room3 name1' },
+//         { name: 'room3 name2' },
+//         { name: 'room3 name3' },
         
-    ],
+//     ],
     
-};
+// };
 
 
 
 
 const getPeople = (obj) => {
     results = [];
-    
+    Object.values(obj).flat().map(function(obj) {
+      results.push(Object.values(obj).flat()[0]);
+    });
     return results;
-};
+  };
 
 
 
 
 
-console.log(getPeople(rooms));
+
+// console.log(getPeople(rooms));
