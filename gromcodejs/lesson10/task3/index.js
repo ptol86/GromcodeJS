6db27d705f848494966885116696273a37e03e37
ptol86 <ah021714@gmail.com>
2020-05-14 14:25:37 +0300

    task

--- gromcodejs/lesson10/task3/index.js
+++ gromcodejs/lesson10/task3/index.js
@@ -2,9 +2,7 @@ const multiRound = num => [
     Math.floor(num * 100) / 100,
     Math.round(num * 100) / 100,
     Math.ceil(num * 100) / 100,
-    Math.trunc(num * 100) / 100,
-    Number.parseInt(((num * 100) / 100).toFixed())
-   
+    Math.trunc(num * 100) / 100, +num.toFixed(2),
 ];
 
 let a = -2.123;
