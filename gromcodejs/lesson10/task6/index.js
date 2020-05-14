09b027e886743aa5eaabf418accdec2ef2bd9a16
ptol86 <ah021714@gmail.com>
2020-05-14 19:02:28 +0300

    task

---
+++ gromcodejs/lesson10/task6/index.js
@@ -0,0 +1,21 @@
+/*input = function-count, min- number, max-number;
+output = array of random numbers from min-number to max-number*/
+
+function getRandomNumbers(count, min, max) {
+    const intNum = Math.ceil(max) === Math.ceil(min); 
+    if (intNum) {
+        return null;
+    }
+
+    let result = []; 
+
+    for (let i = 0; i < count; i++) { 
+        result[i] = Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + Math.ceil(min);
+    };
+    return result;
+}
+
+
+let a = 4.34;
+let b = 233.123;
+let c = 4.34;
