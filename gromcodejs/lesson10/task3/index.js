aa78f7c0e71a32146403385355aa5a32a4fd86aa
ptol86 <ah021714@gmail.com>
2020-05-14 14:18:18 +0300

    task

--- gromcodejs/lesson10/task3/index.js
+++ gromcodejs/lesson10/task3/index.js
@@ -1,10 +1,10 @@
 const multiRound = num => {
     let result = [];
-    result.push(Math.floor(num));
-    result.push(Math.round(num));
-    result.push(Math.ceil(num));
-    result.push(Math.trunc(num));
-    result.push(Number.parseInt((num).toFixed()));
+    result.push(Math.floor(num * 100) / 100);
+    result.push(Math.round(num * 100) / 100);
+    result.push(Math.ceil(num * 100) / 100);
+    result.push(Math.trunc(num * 100) / 100);
+    result.push(Number.parseInt((num * 100) / 100).toFixed());
     return result;
 }
 
