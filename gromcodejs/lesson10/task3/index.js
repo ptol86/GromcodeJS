ebfe7a29c7939e776946fa0d240aced0dcc040f3
ptol86 <ah021714@gmail.com>
2020-05-14 14:10:40 +0300

    task

--- gromcodejs/lesson10/task3/index.js
+++ gromcodejs/lesson10/task3/index.js
@@ -4,8 +4,8 @@ const multiRound = num => {
     result.push(Math.round(num));
     result.push(Math.ceil(num));
     result.push(Math.trunc(num));
-    result.push((num).toFixed());
+    result.push(Number.parseInt((num).toFixed()));
     return result;
 }
 
-let a = 2.123;
+let a = -2.123;
