d238623972138eb205b591e69cb086830f7a91fd
ptol86 <ah021714@gmail.com>
2020-05-16 11:09:19 +0300

    task

---
+++ gromcodejs/lesson11/task4/index.js
@@ -0,0 +1,21 @@
+/*input = string, occurrence;
+output = count of occurrences;
+   algo:
+   1. find out if  string is empty;
+   2. find out count of occurrences is in string;
+   3. return count of occurrences in the string;
+   */
+
+let countOccurrences = (str, occurrence) => {
+    let result =[];
+    count = 0;
+
+    if (str === '') {
+        return null;
+    } 
+    result = [...str.matchAll(occurrence)];
+    console.log(result);
+    count = result.length;
+        return count;
+    }
+    
