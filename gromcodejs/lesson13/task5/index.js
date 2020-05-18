96217ba35aee1000c4f6266ca4d11bed790c27a6
ptol86 <ah021714@gmail.com>
2020-05-18 19:47:17 +0300

    tast

---
+++ gromcodejs/lesson13/task5/index.js
@@ -0,0 +1,72 @@
+export function filterNames(arr, text) {
+       
+    return arr.filter(name => name.includes(text) && name.length > 5);
+}
+
+export function arrAverage(arr) {
+    if (!Array.isArray(arr)) return null;
+    return arr.reduce((sum, current) => sum + current, 0) / arr.length;
+}
+
+export const transaction = {
+    currency: 'USD',
+    value: 170,
+    operation: 'sale',
+    agent: {
+      country: 'Ukraine',
+      company: 'NYSE'
+    },
+    'operation time': 1584029990001
+  };
+  console.log(transaction.currency);
+
+  /* выведите в консоль значение свойства 'currency' с помощью точки */
+  console.log(transaction.value);
+  /* выведите в консоль значение свойства 'value' с помощью квадратных скобок */
+  
+  export const key = 'operation';
+  /* выведите в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
+  console.log(transaction[key]);
+  /* выведите в консоль значение свойства 'company' используя квадратные скобки */
+  console.log(transaction.agent['company']);
+  /* выведите в консоль значение свойства 'country' используя точку */
+  console.log(transaction.agent.country);
+  console.log(transaction["operation time"]);
+  /* выведите в консоль значение свойства 'operation time' используя квадратные скобки */
+
+  export const user = {
+    name: 'Tom',
+    age: 17,
+  };
+  
+  /* присвойте объекту свойство hobby со значением 'football' с помощью точки */
+    user.hobby = "football";
+  
+  export const anotherKey = 'married';
+  /* название свойства хранится в переменной anotherKey. Присвойте объекту такое свойство со значением false */
+    user[anotherKey] = false;
+  /* присвойте объекту свойство 'favorite music' со значением 'rock' */
+    user['favorite music'] = 'rock';
+  /* присвойте объекту свойство 'address', которое будет пустым объкетом */
+    user.address = {};
+  /* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
+    user["address"].country = 'Ukraine';
+  /* присвойте объекту в 'address' свойство 'building', со значением 17 */
+    user["address"]['building'] = 17;
+  /* выведите в консоль объект user */
+  console.log(user);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gromcodejs/lesson13/task5/index.test.js
@@ -0,0 +1,87 @@
+import { filterNames, arrAverage, transaction, key, user, anotherKey } from './index.js';
+
+// function  filterNames
+
+it('should return array elements that includes text and which length > 5', () => {
+    const result = filterNames(['John', 'Olivya', 'Oleksandr', 'Emanuel', 'Vanya', 'Nastya'], 'ya');
+        expect(result).toEqual(['Olivya', 'Nastya']);
+});
+it('should return array elements that includes text and which length > 5', () => {
+    const result = filterNames(['Yellow', 'Green', 'Brown', 'Black', 'Grey', 'White'], 'ow');
+        expect(result).toEqual(['Yellow']);
+});
+it('should return empty array if text not subject to condition', () => {
+    const result = filterNames(['Yellow', 'Green', 'Brown', 'Black', 'Grey', 'White'], 're');
+        expect(result).toEqual([]);
+});
+
+// function arrAverage
+
+it('should get array average number', () => {
+    const result = arrAverage([2, 5, 6, 3]);
+        expect(result).toEqual(4);
+});
+it('should return null if arr is not array', () => {
+    const result = arrAverage("1,2,3,4,5,6");
+        expect(result).toEqual(null);
+});
+it('should get array average number', () => {
+    const result = arrAverage([-1, -2, -3, -4, -5, -6]);
+        expect(result).toEqual(-3.5);
+});
+
+// obj transaction
+
+it('should return transaction currency', () => {
+    const result = transaction.currency;
+    expect(result).toEqual(`USD`);
+});
+it('should return transaction value', () => {
+    const result = transaction.value;
+    expect(result).toEqual(170);
+});
+it('should return  key value', () => {
+    const result = transaction[key];
+    expect(result).toEqual('sale');
+});
+it('should return transaction.agent["company"]', () => {
+    const result = transaction.agent['company'];
+    expect(result).toEqual(`NYSE`);
+});
+it('should return transaction.agent.country', () => {
+    const result = transaction.agent.country;
+    expect(result).toEqual(`Ukraine`);
+});
+it('should return operation time', () => {
+    const result = transaction['operation time'];
+    expect(result).toEqual(1584029990001);
+});
+
+// obj user
+
+it(`'should assign a hobby property to an object with 'football' value using a point'`, () => {
+    const result = user.hobby;
+    expect(result).toEqual("football");
+});
+it(`should assign value of property anotherKey to false`, () => {
+    const result = user[anotherKey];
+    expect(result).toEqual(false);
+});
+it(`'should assign a favorite music property to an object with 'rock' value'`, () => {
+    const result = user['favorite music'];
+    expect(result).toEqual('rock');
+});
+it(`'should assign to obj user addres property with an obj with property country with Ukraine value `, () => {
+    const result = user.address.country;
+    expect(result).toEqual('Ukraine');
+});
+it(`'should assign to  addres property  building with 17 value `, () => {
+    const result = user.address.building;
+    expect(result).toEqual(17);
+});
+
+
+
+
+ 
+ 
