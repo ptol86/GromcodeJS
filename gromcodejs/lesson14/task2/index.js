b6fd15d5b36a86705ac6aac2669322632259565c
ptol86 <ah021714@gmail.com>
2020-05-19 15:33:14 +0300

    test

--- gromcodejs/lesson14/task2/index.js
+++ gromcodejs/lesson14/task2/index.js
@@ -1,23 +1,20 @@
 // 'use strict';
 
-
-
 const createMessenger = () => {
 
     let message = 'Just learn it';
+    let sender = 'Gromcode';
 
     function sendMessage(name) {
         
-        let sender = 'Gromcode';
-    
         console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     function setMessage(text) {
         message = text;
     }
-    function setSender(name) {
-        sender = name;
+    function setSender(test) {
+        sender = test;
     }
     return {
         sendMessage,
@@ -28,7 +25,10 @@ const createMessenger = () => {
 
 const messenger = createMessenger();
 messenger.sendMessage("Bob");
-const messenger2 = createMessenger();
 messenger.sendMessage("Tom");
+const messenger2 = createMessenger();
+messenger2.setMessage("Hello");
+messenger2.setSender("Alex");
+messenger2.sendMessage("July");
 
 export default createMessenger;
