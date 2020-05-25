b5123c7182a3c4ff2480337ce42ea7587c17a7f8
ptol86 <ah021714@gmail.com>
2020-05-25 16:49:32 +0300

    test

--- gromcodejs/lesson18/task1/index.js
+++ gromcodejs/lesson18/task1/index.js
@@ -3,9 +3,10 @@
 export const wallet = {
    transactions: [1, 232, 344, 12, 43, 12],
    getMax() {
-      return Math.max(...this.transactions);
-   },
-   getMin() {
-      return Math.min(...this.transactions);
-   },
+      return Math.max.apply(null, this.transactions);
+
+  },
+  getMin() {
+      return Math.min.apply(null, this.transactions);
+  },
 };
