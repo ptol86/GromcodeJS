16615d8e54209dc93c6ab0fff38ca3a4ae3e0bae
ptol86 <ah021714@gmail.com>
2020-05-26 20:58:04 +0300

    test

--- gromcodejs/lesson19/task3/index.js
+++ gromcodejs/lesson19/task3/index.js
@@ -5,15 +5,15 @@ export function User(name, age) {
   this.name = name;
   this.age = age;
 
-  this.prototype.sayHi = function() {
+  User.prototype.sayHi = function() {
     console.log(`Hi, I am ${this.name}`);
   };
 
-  this.prototype.requestNewPhoto = function () {
+  User.prototype.requestNewPhoto = function () {
     console.log(`New photo request was sent for ${this.name}`);
   };
 
-  this.prototype.setAge = function (newAge) {
+  User.prototype.setAge = function (newAge) {
     if(newAge >= 25) {
       console.log(`New photo request was sent for ${this.name}`);
   }
