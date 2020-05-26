a51555e57dd768207e0544bd8bebbaeda267b179
ptol86 <ah021714@gmail.com>
2020-05-26 20:37:52 +0300

    test

---
+++ gromcodejs/lesson19/task3/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+export function User(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayHi = function() {
+    console.log(`Hi, I am ${this.name}`);
+  };
+  this.requestNewPhoto = function () {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  this.setAge = function (newAge) {
+    if(newAge >= 25) {
+      console.log(`New photo request was sent for ${this.name}`);
+  }
+    if (newAge > 0) {
+    this.age = newAge;
+    return newAge;
+  }
+    else return false;
+  
+};
