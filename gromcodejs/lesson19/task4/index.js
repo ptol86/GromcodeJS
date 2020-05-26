186cb4e5aaf586e792f52e1271e6d9b56807bbf6
ptol86 <ah021714@gmail.com>
2020-05-26 21:25:52 +0300

    test

--- gromcodejs/lesson19/task4/index.js
+++ gromcodejs/lesson19/task4/index.js
@@ -1,30 +1,15 @@
 "use strict";
 
-
-export function User(name, age) {
-  this.name = name;
-  this.age = age;
-
-  User.prototype.sayHi = function() {
-    console.log(`Hi, I am ${this.name}`);
-  };
-
-  User.prototype.requestNewPhoto = function () {
-    console.log(`New photo request was sent for ${this.name}`);
-  };
-
-  User.prototype.setAge = function (newAge) {
-    if(newAge >= 25) {
-      console.log(`New photo request was sent for ${this.name}`);
-  }
-    if (newAge > 0) {
-    this.age = newAge;
-    return newAge;
-  }
-    else return false;
-  }
+export const user = {
+  firstName: "John",
+  lastName: "Doe",
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    const [firstName, lastName] = value.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+    
+  },
 };
-
-const user2 = new User("Bob", 21);
-
-console.log(user2)
