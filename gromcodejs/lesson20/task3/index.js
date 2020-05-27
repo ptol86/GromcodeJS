b9b4cd1974362372e677b9b11038f0e1c950000e
ptol86 <ah021714@gmail.com>
2020-05-27 22:11:34 +0300

    test

---
+++ gromcodejs/lesson20/task3/index.js
@@ -0,0 +1,21 @@
+export class Wallet {
+  constructor() {
+    this._balance = 0;
+  }
+  getBalance() {
+    return this._balance;
+  }
+  deposit(amount) {
+    this._balance += amount;
+  }
+  withdraw(amount) {
+    if (amount > this._balance) {
+      console.log("Not enough funds");
+      return;
+    }
+    this._balance -= amount;
+  }
+}
+
+// const wallet1 = new Wallet();
+// console.log(wallet1._balance);
