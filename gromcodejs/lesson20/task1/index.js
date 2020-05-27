044c6b86aa57e279e9c40469a13ccf57b819d195
ptol86 <ah021714@gmail.com>
2020-05-27 21:50:49 +0300

    test

---
+++ gromcodejs/lesson20/task1/index.js
@@ -0,0 +1,34 @@
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  static createEmpty() {
+    return {
+    name: "",
+    age: null,
+    }
+  }
+  sayHi() {
+  console.log(`Hi, I am ${this.name}`);
+  }
+  requestNewPhoto() {
+  console.log(`New photo request was sent for ${this.name}`);
+  }
+  setAge(value) {
+  if (value < 0) {
+    return false;
+  }
+  this.age = value;
+  if (value >= 25) {
+    this.requestNewPhoto();
+  }
+  return value;
+  }
+}
+// const user1 = new User("tom", 17);
+// console.log(user1);
+
+// console.log(User.createEmpty());
+const user2 = User.createEmpty();
+console.log(user2);
