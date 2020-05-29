52016f1188091d621f729e9631233ca302b3bb42
ptol86 <ah021714@gmail.com>
2020-05-29 13:01:27 +0300

    test

--- gromcodejs/lesson20/task4/index.js
+++ gromcodejs/lesson20/task4/index.js
@@ -1,11 +1,27 @@
 class Order {
   constructor(price, city, type) {
-    id = toString(Math.random());
-    price = price;
-    dateCreated = new Date;
-    isConfirmed = false;
-    dateConfirmed = new Date;
-    city = city;
-    type = type;
-  }
-}
+    this.id = toString(Math.random());
+    this.price = price;
+    this.dateCreated = new Date;
+    this.isConfirmed = false;
+    this.dateConfirmed = new Date;
+    this.city = city;
+    this.type = type;
+  };
+  checkPrice() {
+    if (this.price > 1000) {
+      return true;
+    }
+    else return false;
+  };
+  confirmOrder() {
+    this.isConfirmed = true;
+    this.dateConfirmed = new Date;
+  };
+  isValidType() {
+    if (this.type === "Buy" || this.type === "Sell") {
+      return true;
+    }
+    else return false;
+  };
+};
