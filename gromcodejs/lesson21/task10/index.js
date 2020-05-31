bbcef581417715521c93d91790642524385c844f
ptol86 <ah021714@gmail.com>
2020-05-31 23:40:26 +0300

    test

--- gromcodejs/lesson21/task10/index.js
+++ gromcodejs/lesson21/task10/index.js
@@ -1,14 +1,16 @@
 
 export const finishForm = () => {
   const form = document.querySelector(".login-form");
+
+  const changeAttribute = form.querySelector('input[type="text"]');
+  console.log(changeAttribute);
+  changeAttribute.setAttribute("type", "password");
+
   const login = document.createElement("input");
   login.setAttribute("type", "text"); 
   login.setAttribute("name", "login");
   form.prepend(login);
 
-  const changeAttribute = document.querySelector('input[name="password"]');
-  console.log(changeAttribute);
-  changeAttribute.setAttribute("type", "password");
 }
 
-finishForm();
+// finishForm();
