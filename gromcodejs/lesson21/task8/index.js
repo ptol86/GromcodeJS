0d2d060e47122fe006ac5a7e46ca1bee14e18563
ptol86 <ah021714@gmail.com>
2020-05-31 22:51:39 +0300

    test

--- gromcodejs/lesson21/task8/index.js
+++ gromcodejs/lesson21/task8/index.js
@@ -5,21 +5,22 @@ export const finishList = () => {
   firstEl.textContent = "1";
   listElements.prepend(firstEl);
 
-  const fourEl = document.createElement("li");
-  const three = document.querySelector(".three");
-  fourEl.textContent = "4";
-  three.after(fourEl);
-
-  const sixEl = document.createElement("li");
-  const seven = document.querySelector(".seven");
-  sixEl.textContent = "6";
-  seven.before(sixEl);
-  
   const eightEl = document.createElement("li");
   eightEl.textContent = "8";
   listElements.appendChild(eightEl);
+
+  const specialEl = document.querySelector(".special");
+  
+  const fourEl = document.createElement("li");
+  fourEl.textContent = "4";
+  specialEl.before(fourEl);
+
+  const sixEl = document.createElement("li");
+  sixEl.textContent = "6";
+  specialEl.after(sixEl);
+    
  }
 
-finishList();
+// finishList();
 
 
