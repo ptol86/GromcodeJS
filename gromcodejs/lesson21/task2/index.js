0f1e93c601fe5279b83f34cfb836e57675cc6c79
ptol86 <ah021714@gmail.com>
2020-05-31 21:07:26 +0300

    test

---
+++ gromcodejs/lesson21/task2/index.js
@@ -0,0 +1,17 @@
+
+export const getTitleElement = () => {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+}
+
+// getTitleElement();
+
+export const getInputElement = () => {
+  const inputElem = document.querySelector("input");
+  console.dir(inputElem);
+  return inputElem;
+}
+
+// getInputElement();
+
