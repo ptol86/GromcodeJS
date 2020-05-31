22a5954cc053a27f3e8c5a4fd5a410e6612335e9
ptol86 <ah021714@gmail.com>
2020-05-31 23:58:16 +0300

    test

---
+++ gromcodejs/lesson21/task11/index.js
@@ -0,0 +1,17 @@
+export const manageClasses = () => {
+  const first = document.querySelector(".one");
+  first.classList.add("selected");
+
+  const two = document.querySelector(".two");
+  two.classList.remove("selected");
+
+  const three = document.querySelector(".three");
+  three.classList.toggle("three_done");
+
+  const four = document.querySelector(".four");
+  if ([...four.classList].includes("some-class")) {
+  four.classList.add("another-class");
+  }
+}
+
+manageClasses();
