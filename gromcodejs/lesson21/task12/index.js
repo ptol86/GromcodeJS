93cb88f764d42da84f7dfb339b125b1e22cc4b25
ptol86 <ah021714@gmail.com>
2020-06-01 00:26:03 +0300

    test

---
+++ gromcodejs/lesson21/task12/index.js
@@ -0,0 +1,11 @@
+export const squaredNumbers = () => {
+  const list = Array.from(document.querySelectorAll(".number"));
+
+  list.forEach((el) => {
+    const squaredNumber = el.dataset.number**2;
+    el.dataset.dataSquaredNumber = squaredNumber;
+  });
+  
+}
+
+squaredNumbers();
