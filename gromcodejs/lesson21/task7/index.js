1509f6ff586de096a8977daa612a9bda66cbb2d8
ptol86 <ah021714@gmail.com>
2020-05-31 22:27:14 +0300

    test

---
+++ gromcodejs/lesson21/task7/index.js
@@ -0,0 +1,12 @@
+
+ const createButton = (buttonText) => {
+  const body = document.querySelector("body");
+  const bodyElem = document.createElement("button");
+  bodyElem.textContent = buttonText;
+  body.append(bodyElem);
+  
+ }
+
+ createButton("some text");
+
+
