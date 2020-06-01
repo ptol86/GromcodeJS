08c5da75c7e06e28676f11e78b5eb7dd257b2185
ptol86 <ah021714@gmail.com>
2020-06-01 10:44:29 +0300

    test

--- gromcodejs/standups/objects/task4/index.js
+++ gromcodejs/standups/objects/task4/index.js
@@ -1,25 +1,27 @@
 function markAdmins(users, adminIds) {
-   return users.map(user => {
-     const isAdmin = adminIds.includes(user.id);
- 
+  return users.map(user => {
+     let isAdmin = adminIds.includes(user.id);
      return { isAdmin, ...user }
-   })
- }
- 
- const adminIds = ['1', '3'];
- const usersInf = [
-     { id: '1',  name: 'Bob' },
-     { id: '2', name: 'Tom' },
-     { id: '3', name: 'Sam' },
-     { id: '4', name: 'Tad' }
- ];
- const adminUsers = markAdmins(usersInf, adminIds);
- console.log(adminUsers);
- // result
- // [
- //    { id: '1', isAdmin: true, name: 'Bob' },
- //    { id: '2', isAdmin: false, name: 'Tom' },
- //    { id: '3', isAdmin: true, name: 'Sam' },
- //    { id: '4', isAdmin: false, name: 'Tad' }
- // ];
- 
+   
+  });
+}
+// function markAdmins1(users, adminIds) {
+//   return users.map(user => ({...user, isAdmin: adminIds.includes(user.id)}));
+// }
+
+// const adminIds = ['1', '3'];
+// const usersInf = [
+//     { id: '1',  name: 'Bob' },
+//     { id: '2', name: 'Tom' },
+//     { id: '3', name: 'Sam' },
+//     { id: '4', name: 'Tad' }
+// ];
+// const adminUsers = markAdmins1(usersInf, adminIds);
+// console.log(adminUsers);
+//  result
+//  [
+//     { id: '1', isAdmin: true, name: 'Bob' },
+//     { id: '2', isAdmin: false, name: 'Tom' },
+//     { id: '3', isAdmin: true, name: 'Sam' },
+//     { id: '4', isAdmin: false, name: 'Tad' }
+//  ];
