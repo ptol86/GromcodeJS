68b0d8e20153d3e7da523715e16e13dc3f727248
ptol86 <ah021714@gmail.com>
2020-06-04 12:28:00 +0300

    test

--- gromcodejs/lesson22/task11/index.js
+++ gromcodejs/lesson22/task11/index.js
@@ -1,90 +1,103 @@
-const tasks = [
-  { text: 'Buy milk', done: true },
-  { text: 'Pick up Tom from airport', done: false },
-  { text: 'Visit party', done: false },
-  { text: 'Visit doctor', done: true },
-  { text: 'Buy meat', done: true },
+// 1. create array of tasks - and create 5 test tasks +++
+// 2. write renderTasks (tasks) fucn that render all tasks +++
+// 3. addEventListener for .list +++
+// 4. addEventListener for create button+++
+// 5. checkbox handler - update task in the array and render tasks
+// 6. create handler - create task object and put to the array and render tasks +++
+
+let tasks = [
+  { text: 'Buy milk', done: false, id: "1"},
+  { text: 'Pick up Tom from airport', done: false, id: "2"}, 
+  { text: 'Visit party',  done: false, id: "3"},
+  { text: 'Visit doctor', done: true, id: "4"},
+  { text: 'Buy meat', done: true, id: "5"},
 ];
 
+const listElem = document.querySelector('.list');
 
-const renderListItems = listItems => {
-  const listElem = document.querySelector(".list");
+const renderTasks = (tasksList) => {
+listElem.innerHTML = '';
+  const tasksElems = tasksList
+      .slice()
+      .sort((a, b) => a.done - b.done)
+      .map((task) => {
+          const listItemElem = document.createElement('li');
+          listItemElem.classList.add('list__item');
+          const checkbox = document.createElement('input');
+          checkbox.setAttribute('type', 'checkbox');
+          checkbox.setAttribute('data-task-id', task.id);
+          checkbox.checked = task.done;
+          checkbox.classList.add('list__item-checkbox');
+          if (task.done) {
+              listItemElem.classList.add('list__item_done');
+          }
+          listItemElem.append(checkbox, task.text);
+          return listItemElem;
+      });
+      console.log(tasksElems)
+  listElem.append(...tasksElems);
+};
+// renderTasks(tasks);
+//update task
+listElem.addEventListener('click', updateTask);
 
-  const listItemsElems = listItems
-  .sort((a, b) => a.done - b.done)
-  .map(({text, done}) => {
-    const listItemElem = document.createElement("li");
-    listItemElem.classList.add("list__item");
-    listItemElem.setAttribute("data-id", null);
-    if (done) {
-      listItemElem.classList.add("list__item_done");
-    }
-    const checkBoxElem = document.createElement("input");
-    checkBoxElem.setAttribute("type", "checkbox");
-    checkBoxElem.checked = done;
-    checkBoxElem.classList.add("list__item-checkbox");
-    listItemElem.append(checkBoxElem, text)
-    
-    return listItemElem;
-  });
-  
-  listElem.append(...listItemsElems);
-}
-
-renderListItems(tasks);
-
-const createBtn = document.querySelector('.create-task-btn')
-const taskInput = document.querySelector('.task-input')
-
-const createTask = () => {
-    if (taskInput.value === "") {
-        return;
-    };
-    tasks.push({
-        text: taskInput.value,
-        done: false,
-        id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-    });
-    taskInput.value = "";
-    tasks.value = "";
-    // listElem.innerHTML = "";
-    renderListItems(tasks);
+function updateTask(event) {
+  // console.log(event);
+  //console.log(event.target);
+  const classes = event.target.classList;
+  // console.log(classes)
+  if(!classes.contains('list__item-checkbox')) return;
+  // console.log(event.target.dataset);
+  const task = tasks.find(task => task.id === event.target.dataset.taskId);
+  if (task) {
     tasks.map(el => {
-      documentlistItemElem.getAttribute(dataset.id) = el.id;
+      if (el.id == event.target.dataset.taskId) {
+        el.done = true;
+      }
     });
-    console.log(tasks);
+    renderTasks(tasks);
+  }
+  // console.log(task);
+  //todo update task
+  //updateTask();
+  //todo render tasks again
+  //renderTasks(tasks);
+  // console.log('DONE');
 }
-
+//renderTasks(tasks);
+const createBtn = document.querySelector('.btn');
 createBtn.addEventListener('click', createTask);
 
-const checkBox = document.querySelector(`.list__item-checkbox`);
+function createTask(event) {
+  // console.log(event);
+  const input = document.querySelector('.task-input');
+if (!input.value) return;
+  const newTask = {
+      text: input.value,
+      done: false,
+      id: toString(Math.floor(Math.random() * (1000 - 1 + 1)) + 1),
+  }
+  tasks.push(newTask);
+  input.value = '';
+  renderTasks(tasks);
+  // console.log(tasks); 
+}
 
-const checkboxClick = (event) => {
-  tasks.map(el => {
-    if (el.id === target.dataset.id) {
-        return {...el, done: true};
-      }
-      return el;
-    })
-};
-
-// checkBox.addEventListener("change", checkboxClick);
 
 
-
-/*
-    algo: 
-    1. добавить обработчик на кнопку
-    2. внутри обработчика необходимо прочитать значение поля инпут
-    3. запушить значение с уникальным айдишником и значением фолс
-    4. засунуть ай-ди в дата атрибут
-    5. поставить обработчик на чекбокс прочитать его таргет
-    6. найти значение ай-ди на кликнутом чекбоксе 
-    7. найти чекнутый елемент в массиве таск и поменять ему статус дан
+// /*
+//     algo: 
+//     1. добавить обработчик на кнопку
+//     2. внутри обработчика необходимо прочитать значение поля инпут
+//     3. запушить значение с уникальным айдишником и значением фолс
+//     4. засунуть ай-ди в дата атрибут
+//     5. поставить обработчик на чекбокс прочитать его таргет
+//     6. найти значение ай-ди на кликнутом чекбоксе 
+//     7. найти чекнутый елемент в массиве таск и поменять ему статус дан
     
-*/ 
-    // tasks.push({
-    //   text: Input.value, 
-    //   done: false,
-    //   id: Math.random(),
-    // })
+// */ 
+//     // tasks.push({
+//     //   text: Input.value, 
+//     //   done: false,
+//     //   id: Math.random(),
+//     // })
---
+++ gromcodejs/standups/executioncontext/task1/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+export const timer = {
+   secondsPassed: 0,
+   minsPassed: 0,
+   intervalId: 0, 
+
+   startTimer: function startTimer() {
+      this.intervalId = setInterval(function() {
+      this.secondsPassed++;
+      if (this.secondsPassed === 60) {
+          this.secondsPassed = 0;
+          this.minsPassed++;
+      }
+      console.log(this.secondsPassed);
+   }.bind(this), 1000);
+   },
+   
+   getTime() {
+      if (this.secondsPassed < 10) {
+         return `${this.minsPassed}:0${this.secondsPassed}`; 
+      }
+      return `${this.minsPassed}:${this.secondsPassed}`;
+      
+   },
+
+   stopTimer() {
+      clearInterval(this.intervalId);
+   },
+
+   resetTimer() { 
+      this.secondsPassed = 0;
+      this.minsPassed = 0;
+   },
+
+}
+
+
