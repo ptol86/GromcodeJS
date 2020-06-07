9857fc233793e831ff3c3b37ce398147bfb19a9c
ptol86 <ah021714@gmail.com>
2020-06-07 13:29:43 +0300

    test

--- gromcodejs/lesson24/task1/index.js
+++ gromcodejs/lesson24/task1/index.js
@@ -1,6 +1,6 @@
-const weekDays = ["Su", "Mo", "Th", "We", "Th", "Fr", "St"]
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
+  const weekDays = ["Su", "Mo", "Th", "We", "Th", "Fr", "St"]
   const day = new Date(date).getDate();
   console.log(day)
   const dateInFuture = new Date(date).setDate(day + days);
