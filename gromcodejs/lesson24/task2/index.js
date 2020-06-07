f69c7a3e9baaa48f3e61350de3382fa9d75da033
ptol86 <ah021714@gmail.com>
2020-06-07 20:07:04 +0300

    test

--- gromcodejs/lesson24/task2/index.js
+++ gromcodejs/lesson24/task2/index.js
@@ -5,7 +5,7 @@ const formatter = new Intl.DateTimeFormat("en", {
   hour12: false,
 });
 
-export const getGreenwitchTime = date => formatter.format(date);
+export const getGreenwichTime = date => formatter.format(date);
 
 
 // console.log(getTime(new Date()));
