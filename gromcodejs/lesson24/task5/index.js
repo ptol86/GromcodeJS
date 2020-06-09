534d0a38650cc5f89c1df6711cb3bc166ab67d46
ptol86 <ah021714@gmail.com>
2020-06-09 10:54:24 +0300

    test

---
+++ gromcodejs/lesson24/task5/index.js
@@ -0,0 +1,6 @@
+studentsBirthDays(students)
+
+[{ name: 'Tom', birthDate: '01/15/2010' }, { name: 'Ben', birthDate: '01/17/2008' }, { name: 'Sam', birthDate: '03/15/2010' }]
+
+
+{ Jan: ['Tom', 'Ben'], Mar: ['Sam'] }
--- gromcodejs/standups/executioncontext/task1/index.js
+++ gromcodejs/standups/executioncontext/task1/index.js
@@ -1,32 +1,54 @@
 "use strict";
+/*
+1. learn requirements +++
+2. create step by step algo (&input/output for functions)
+3. create prototype solution
+4. testing
+5. refactoring
+6. final solution
+*/ 
+// let secondsPassed = 10;
 
-export const timer = {
+const timer = {
    secondsPassed: 0,
    minsPassed: 0,
-   intervalId: 0, 
+   
+  //  startTimer() {
 
-   startTimer: function startTimer() {
-      this.intervalId = setInterval(function() {
-      this.secondsPassed++;
-      if (this.secondsPassed === 60) {
+          // loose context
+  //     const adder = function() {
+  //       this.secondsPassed += 1;
+  //        console.log(this.secondsPassed);
+  //     };
+        // fix context
+  //   const newAdder = adder.bind(this);
+
+  //   setInterval(newAdder, 1000);
+  //  },
+
+   startTimer() {
+      let intervalId = setInterval(() => {
+        this.secondsPassed += 1;
+        //  console.log(this.secondsPassed);
+        if (this.secondsPassed === 60) {
+          this.minsPassed += 1;
           this.secondsPassed = 0;
-          this.minsPassed++;
-      }
-      console.log(this.secondsPassed);
-   }.bind(this), 1000);
+        }
+        // console.log(this.secondsPassed);
+        // console.log(this.minsPassed);
+    }, 1000);
    },
    
-   getTime() {
-      if (this.secondsPassed < 10) {
-         return `${this.minsPassed}:0${this.secondsPassed}`; 
-      }
-      return `${this.minsPassed}:${this.secondsPassed}`;
-      
+   stopTimer() {
+     clearInterval(this.intervalId);
    },
 
-   stopTimer() {
-      clearInterval(this.intervalId);
-   },
+   getTime() {
+      return this.secondsPassed < 10 
+        ? `${this.minsPassed}:0${this.secondsPassed}`
+        : `${this.minsPassed}:${this.secondsPassed}`;
+    },
+   
 
    resetTimer() { 
       this.secondsPassed = 0;
@@ -34,5 +56,3 @@ export const timer = {
    },
 
 }
-
-
