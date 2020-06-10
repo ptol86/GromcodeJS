257fc65169dad80d6f5a8669b8243460386e6969
ptol86 <ah021714@gmail.com>
2020-06-10 16:46:03 +0300

    task

--- gromcodejs/lesson27/task1/index.js
+++ gromcodejs/lesson27/task1/index.js
@@ -1,5 +1,24 @@
 localStorage.clear();
 localStorage.setItem("name", "Tom");
 localStorage.setItem("hobbies", JSON.stringify({ name: "Tom"}));
+localStorage.setItem("age", JSON.stringify(17));
 
-console.log(JSON.parse(localStorage.getItem("hobbies")));
+// console.log(JSON.parse(localStorage.getItem("hobbies")));
+
+const getLocalStorageData = () => {
+  return Object.entries(localStorage)
+  .reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch(e) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
+
+console.log(getLocalStorageData());
---
+++ gromcodejs/standups/executioncontext/task2/index.js
@@ -0,0 +1,58 @@
+"use strict";
+/*
+1. learn requirements +++
+2. create step by step algo (&input/output for functions)
+3. create prototype solution
+4. testing
+5. refactoring
+6. final solution
+*/ 
+// let secondsPassed = 10;
+
+export const timer = {
+   secondsPassed: 0,
+   minsPassed: 0,
+   intervalId: null,
+  //  startTimer() {
+
+          // loose context
+  //     const adder = function() {
+  //       this.secondsPassed += 1;
+  //        console.log(this.secondsPassed);
+  //     };
+        // fix context
+  //   const newAdder = adder.bind(this);
+
+  //   setInterval(newAdder, 1000);
+  //  },
+
+   startTimer() {
+      this.intervalId = setInterval(() => {
+        this.secondsPassed += 1;
+        //  console.log(this.secondsPassed);
+        if (this.secondsPassed === 60) {
+          this.minsPassed += 1;
+          this.secondsPassed = 0;
+        }
+        // console.log(this.secondsPassed);
+        // console.log(this.minsPassed);
+    }, 1000);
+   },
+   
+   stopTimer() {
+     clearInterval(this.intervalId);
+   },
+
+   getTime() {
+      return this.secondsPassed < 10 
+        ? `${this.minsPassed}:0${this.secondsPassed}`
+        : `${this.minsPassed}:${this.secondsPassed}`;
+    },
+   
+
+   resetTimer() { 
+      this.secondsPassed = 0;
+      this.minsPassed = 0;
+   },
+
+}
