5e1ea24400512f8e49e45a64aaecee499f3930ee
ptol86 <ah021714@gmail.com>
2020-06-11 20:04:31 +0300

    task

--- gromcodejs/standups/executioncontext/task2/index.js
+++ gromcodejs/standups/executioncontext/task2/index.js
@@ -1,58 +1,30 @@
-"use strict";
-/*
-1. learn requirements +++
-2. create step by step algo (&input/output for functions)
-3. create prototype solution
-4. testing
-5. refactoring
-6. final solution
-*/ 
-// let secondsPassed = 10;
-
-export const timer = {
-   secondsPassed: 0,
-   minsPassed: 0,
-   intervalId: null,
-  //  startTimer() {
-
-          // loose context
-  //     const adder = function() {
-  //       this.secondsPassed += 1;
-  //        console.log(this.secondsPassed);
-  //     };
-        // fix context
-  //   const newAdder = adder.bind(this);
-
-  //   setInterval(newAdder, 1000);
-  //  },
-
-   startTimer() {
-      this.intervalId = setInterval(() => {
-        this.secondsPassed += 1;
-        //  console.log(this.secondsPassed);
-        if (this.secondsPassed === 60) {
-          this.minsPassed += 1;
-          this.secondsPassed = 0;
-        }
-        // console.log(this.secondsPassed);
-        // console.log(this.minsPassed);
-    }, 1000);
-   },
-   
-   stopTimer() {
-     clearInterval(this.intervalId);
-   },
-
-   getTime() {
-      return this.secondsPassed < 10 
-        ? `${this.minsPassed}:0${this.secondsPassed}`
-        : `${this.minsPassed}:${this.secondsPassed}`;
-    },
-   
-
-   resetTimer() { 
-      this.secondsPassed = 0;
-      this.minsPassed = 0;
-   },
-
+function printMessage(age, city) {
+  console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
 }
+
+let user = {
+  name: "Alex",
+  email: "gmail.com",
+}
+
+function myBind(func, context) {
+  let copyContext = {...context};
+  copyContext[func.name] = func;
+  console.log(func.name)
+  return (...args) => {
+    copyContext[func.name](...args)
+  }
+}
+
+
+
+function myBindWithApply(func, context) {
+  return function(...args) {
+    let funcResult = func.apply(context, args);
+    return funcResult;
+  }
+}
+
+const bindedPrintMessage = myBindWithApply(printMessage, user);
+
+bindedPrintMessage(33, "Kyiv");
