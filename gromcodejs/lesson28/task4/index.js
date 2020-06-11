1e4c48793ee73456f0998136d33dbf711ddf3641
ptol86 <ah021714@gmail.com>
2020-06-11 21:54:23 +0300

    task

--- gromcodejs/lesson28/task4/index.js
+++ gromcodejs/lesson28/task4/index.js
@@ -1,18 +1,18 @@
 //compose
 
-const add3 = value => value +3;
-const mult2 = value => value * 2;
-const div4 = value => value / 4;
+// const add3 = value => value +3;
+// const mult2 = value => value * 2;
+// const div4 = value => value / 4;
 
 
-const compose = (...funcs) => value => {
+export const compose = (...funcs) => value => {
     return funcs.reduce((acc, func) => func(acc), value)
 }
 
-const doEverything = compose(
-    add3,
-    mult2,
-    div4,
-);
+// const doEverything = compose(
+//     add3,
+//     mult2,
+//     div4,
+// );
 
-console.log(doEverything(3));
+// console.log(doEverything(3));
