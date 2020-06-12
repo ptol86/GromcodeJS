495ed2a0c53096f52074b5aabb8d3d3faffcfa81
ptol86 <ah021714@gmail.com>
2020-06-12 13:27:52 +0300

    task

--- gromcodejs/standups/executioncontext/task2/index.js
+++ gromcodejs/standups/executioncontext/task2/index.js
@@ -28,3 +28,51 @@ function myBindWithApply(func, context) {
 const bindedPrintMessage = myBindWithApply(printMessage, user);
 
 bindedPrintMessage(33, "Kyiv");
+/*
+1. learn requirements +++
+2. create step by step algo (&input/output for functions)
+3. create prototype solution
+4. testing
+5. refactoring
+6. final solution
+*/ 
+
+function printMessage(age, city) {
+  // console.log(this);
+  console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
+}
+
+let user = {
+  name: "Alex",
+  email: "gmail.com",
+}
+
+
+// printMessage(50, "Kyiv");
+
+// let bindedPrintMessage = printMessage.bind(user);
+// bindedPrintMessage(50, "Kyiv");
+
+//input: func, obj
+//output: func with bind
+
+// function myBind1(func, context) {
+//   return func.bind(context);
+// }
+
+// let bindedPrintMessage = myBind(printMessage, user);
+// bindedPrintMessage(50, "Kyiv");
+//TODO implement it
+
+const myBind2 = (func, context) => {
+  context[func.name] = func;
+  console.log(context)
+  return context[func.name];
+}
+// user.printMessage = printMessage;
+// console.log(user.printMessage(55, "Melbourne"));
+
+let bindedPrintMessage = myBind2(printMessage, user);
+
+// console.log(bindedPrintMessage);
+bindedPrintMessage(55, "Lviv");
