81b19f8b2ea4e23996a159150a173c1bdc043b12
ptol86 <ah021714@gmail.com>
2020-06-13 18:36:30 +0300

    task

--- gromcodejs/lesson29/task3/index.js
+++ gromcodejs/lesson29/task3/index.js
@@ -1,11 +1,13 @@
-const main = () => {
+export const printing = () => {
     console.log("1");
     setTimeout(() => {
-        console.log("2");
-    }, 0);
-    console.log("3");
+        console.log("5");
+    }, 1000);
+    console.log("2");
     setTimeout(() => {
         console.log("4");
-    }, 1000);
-    console.log("5");
+    }, 0);
+    console.log("3");
 }
+
+// printing();
