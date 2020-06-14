28e118b16d32e34c5cbed7bb4b1c6212f6895cd3
ptol86 <ah021714@gmail.com>
2020-06-14 15:02:20 +0300

    task

---
+++ gromcodejs/lesson30/task1/index.js
@@ -0,0 +1,45 @@
+//add image
+export const addImage = imgSrc => {
+    const p = new Promise((resolve, reject) => {
+        const imgElem = document.createElement('img');
+    imgElem.setAttribute("alt", "My Photo");
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector(".page");
+
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+        const {width, height} = imgElem;
+        console.log(width, height);
+        resolve(null, { width, height });
+    }
+    imgElem.addEventListener("load", onImageLoaded);
+    
+    imgElem.addEventListener("error", () => reject(new Error("Image load failed")));
+    })
+    return p;
+}
+
+
+
+const imgSrc = "https://grocode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson22/hw2/example-599px.jpg";
+
+const resultPromise = addImage(imgSrc);
+
+resultPromise.then(data => console.log(data));
+
+resultPromise.catсh(error => console.log(error));
+
+
+// const onImageLoadedd = (error, data) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+//     const { width, height } = data;
+//     const sizeElem = document.querySelector(".image-size");
+    
+//     sizeElem.textContent = `${width} x ${height}`;
+// }
+
+// addImage(imgSrc, onImageLoadedd);
