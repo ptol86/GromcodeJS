97c75bef8d763fdb206ca5439010a23b4dd4b1b6
ptol86 <ah021714@gmail.com>
2020-06-14 16:07:13 +0300

    task

--- gromcodejs/lesson29/task4/index.js
+++ gromcodejs/lesson29/task4/index.js
@@ -3,24 +3,25 @@ export const requestUserData = (userId, callback) => {
   setTimeout(() => { 
     if (userId === "broken") {
         callback(`Failed to load user data`);
+        return;
     }
 
-    const a = () => {
-       return callback(userId);
+    const a = (callback, userId) => {
+      return callback(userId);
       // console.log(userId);
       // console.log(callback);
     }
     a();
-    // console.log(a);
+    console.log(a);
 
   }, Math.ceil(Math.random()*3000));
 }
 
-let callback1 = (userId) => {
-    return {userId: userId,
-      email: `${userId}@example.com`}
+const callback1 = (arg) => {
+    return {userId: arg,
+      email: `${arg}@example.com`}
 };
 
 // console.log(callback1("vasilyi"));
-  // requestUserData("Bob", callback1);
-// console.log(requestUserData("Bob", callback1));
+// requestUserData("Bob", callback1);
+console.log(requestUserData("Bob", callback1));
