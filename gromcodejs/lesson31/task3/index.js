0bf5b6a18311266f68f722d587064db22469534a
ptol86 <ah021714@gmail.com>
2020-06-15 14:56:33 +0300

    task

--- gromcodejs/lesson31/task3/index.js
+++ gromcodejs/lesson31/task3/index.js
@@ -1,26 +1,34 @@
-// asyncCalculator
+const successPromise = new Promise(resolve => {
+  resolve(32);
+});
 
-export const asyncCalculator = num => 
-  new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(`Initial value: ${num}`)
-      resolve(num);
-    }, 500);
+/*
+* исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+*/
 
+successPromise
+  .then(number => {
+      const halfNumber = number / 2;
+      return halfNumber;
   })
-  .then(value =>  new Promise((resolve) => {
-      setTimeout(() => {
-        const result = value **2
-        console.log(`Squared value: ${result}`)
-        resolve(result);
-      }, 500);
+  .then(number => {
+      const squaredNumber = number * number;
+      return squaredNumber;
+  })
+  .then(result => {
+      console.log(result); // 256
+  });
 
-    }))
-    .then(value => {
-      const result = value * 2
-      console.log(`Doubled value: ${result}`)
-      return result;
-    })
- 
-// asyncCalculator(4)
-//     .then(value => console.log(value))
+/*
+* исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+*/
+successPromise
+  .then(number => {
+      const lastNum = number * 10
+      return lastNum;
+  })
+  .then(result => {
+      console.log(result); // 320
+  });
+
+console.log('!!! Обратите внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!');
