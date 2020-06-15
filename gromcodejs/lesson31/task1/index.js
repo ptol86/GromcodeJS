7ff2a4716157116668a8ef5721c797edf2dd905c
ptol86 <ah021714@gmail.com>
2020-06-15 14:38:29 +0300

    task

---
+++ gromcodejs/lesson31/task1/index.js
@@ -0,0 +1,26 @@
+export const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+      if (userId === "broken") {
+        setTimeout(() => {
+          reject(new Error("User not found"))
+        }, 500);
+      } else setTimeout(() => {
+        resolve({
+          name: "John",
+          age: 17,
+          email: `${userId}`,
+          userId
+        })
+      }, 1000);
+    })
+    return request;
+
+ }
+
+//  requestUserData("user-1")
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+//   .finally(() => console.log("finally"));
+
+//  requestUserData("broken-1")
+//  .then(data => console.log(data));
