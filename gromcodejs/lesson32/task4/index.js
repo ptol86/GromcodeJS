940d95ffeb7179250e68bac914d39536a2f803a1
ptol86 <ah021714@gmail.com>
2020-06-16 16:39:00 +0300

    task

---
+++ gromcodejs/lesson32/task4/index.js
@@ -0,0 +1,24 @@
+const promiseNumber1 = Promise.resolve(67);
+const promiseNumber2 = Promise.resolve(23);
+const promiseNumber3 = Promise.resolve(8);
+
+/*
+ * создайте промис и присвойте переменной resultPromise
+ * чтобы в консоль вывелась сумма всех чисел из трех промисов
+ */
+const resultPromise = (...numbers) => {
+  console.log(Promise.all(numbers))
+  return Promise.all(numbers);
+
+};
+resultPromise(promiseNumber1, promiseNumber2, promiseNumber3)
+    .then(numbersList => {
+        console.log(numbersList);
+        const sum = numbersList.reduce((acc, num) => acc + num, 0);
+        return sum;
+    })
+    .then(result => {
+        console.log(result); // 98
+    });
+
+// resultPromise(promiseNumber1, promiseNumber2, promiseNumber3)
