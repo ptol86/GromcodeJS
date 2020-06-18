a226f4dbf7c6bb113cb3660f61967a2b349cc109
ptol86 <ah021714@gmail.com>
2020-06-18 19:15:27 +0300

    task

--- gromcodejs/lesson33/task2/index.js
+++ gromcodejs/lesson33/task2/index.js
@@ -3,15 +3,15 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 /* getTasksList code here */
 export const getTasksList = () => {
-  return fetch('https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks')
+  return fetch(baseUrl)
   .then(response => response.json())
  
 }
-// console.log(getTasksList());
+console.log(getTasksList());
 
 export const getTaskById = taskId => {
-  return getTasksList()
-  .then(task => task[taskId-1]);
+  return fetch(`${baseUrl}/${taskId}`)
+  .then(response => response.json())
 }
 
-// console.log(getTaskById(1));
+console.log(getTaskById(1));
