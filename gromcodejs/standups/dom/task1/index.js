1cbb7abc9aafd61914b7e764810f1537e4d948b9
ptol86 <ah021714@gmail.com>
2020-06-22 10:11:59 +0300

    test5

--- gromcodejs/standups/dom/task1/index.js
+++ gromcodejs/standups/dom/task1/index.js
@@ -1,12 +1,12 @@
-const setButton = (buttonText) => {
+export const setButton = (buttonText) => {
   // part 1  
   // const button = `<button>${buttonText}</button>`;
 
     // document.querySelector("body").innerHTML = button;
     const button = document.createElement("button");
     button.textContent = buttonText;
-    
+
     document.querySelector("body").append(button);
   }
 
-setButton();
+// setButton();
