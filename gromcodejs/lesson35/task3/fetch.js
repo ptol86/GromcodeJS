374ec658536f186afbfaa98e62bf53cb8a3de38c
ptol86 <ah021714@gmail.com>
2020-06-24 23:03:03 +0300

    test

---
+++ gromcodejs/lesson35/task3/fetch.js
@@ -0,0 +1,23 @@
+export const fetchUserData = userName => {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then(response => {
+            if (response.ok) {
+                return  response.json();
+            }
+            else {
+                throw new Error("Failed to load data")
+            };
+        });
+}
+
+export const fetchReposUrlData = url => {
+    return fetch(url)
+        .then(response =>  {
+            if (response.ok) {
+                return  response.json();
+            }
+            else {
+                throw new Error("Failed to load data")
+            };
+        });
+}
