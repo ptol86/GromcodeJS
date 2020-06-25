f0dc2497699d2f80d40c92ae96af7eca3d7f688a
ptol86 <ah021714@gmail.com>
2020-06-25 18:11:23 +0300

    test

---
+++ gromcodejs/lesson36/task1/index.js
@@ -0,0 +1,17 @@
+export const fetchUser = async userID => {
+    try {
+        const response = await fetch(`ttps://api.github.com/users/${userID}`);
+        if (!response.ok) {
+            return null;
+        }
+        const userData = await response.json();
+        return userData;
+    } catch (err) {
+        throw new Error("Failed to fetch user");
+    }
+};
+
+fetchUser("github")
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message));
+
